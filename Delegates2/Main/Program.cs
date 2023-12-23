9035308eef05bd1b19c8b20b440c52066aaf98a7
Abhi <144783688+Abhijitt25@users.noreply.github.com>
2023-12-23 19:03:33 +0530

    Mainfile

---
+++ Delegates2/Main/Program.cs
@@ -0,0 +1,15 @@
+﻿using bookrecord;
+using ackDeligate;
+using acknowledge;
+
+Books b=new Books();
+
+b.NoOfCopies=5;
+b.borrow("Thremodynamics","Physics",4);
+
+
+Acknowledge a=new Acknowledge();
+AckDeligates sd=new AckDeligates(a.msg);
+b.msg+=a.msg;
+b.NoOfCopies=4;
+b.borrow("RDSHARMA","Maths",6);
